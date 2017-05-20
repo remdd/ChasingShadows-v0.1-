9c3ec9d3ef6dbde14e6a73efea0182f8de64e976
remdd <munkrat@gmail.com>
2017-05-20 23:37:49 +0100

    Add portfolio gallery panels

--- assets/js/chasingshadows.js
+++ assets/js/chasingshadows.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-	$("#welcomeSplashDiv").click(function(){
-		window.scrollBy({
-			top: 100%
+	$("#obscurer").on("click", function() {
+		$('#welcomeSplashDiv').fadeOut(500, function() {
+			$('#welcomeSplashDiv').remove();
+		$('#obscurer').remove();
 		});
+	});
+	$(".galleryCover").fadeTo(0, 0.6);
+	$(".galleryTitle").fadeTo(0, 0);
+	$(".galleryCover").hover(function() {
+		$(this).fadeTo(500, 1);
+		$(this).children(":first").fadeTo(500, 1);
+	}, function() {
+		$(this).fadeTo(500, 0.6);
+		$(this).children(":first").fadeTo(500, 0);
 	});
 });
 
